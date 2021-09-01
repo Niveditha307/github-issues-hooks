b64299591373c8673932689a924426db2e64ed05
Niveditha <nivedithareddy307@gmail.com>
2021-09-01 12:06:26 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { Route,Switch,BrowserRouter } from 'react-router-dom';
 import GitHubIssuesList from "./components/GitHubIssuesList"
-//import GitHubIssuesListItem from './components/GitHubIssuesListItem';
-// import About from './components/About';
-// import Contact from './components/Contact';
-// import NotFound from './components/NotFound';
+import GitHubIssuesListItem from './components/GitHubIssuesListItem';
+import About from './components/About';
+import Contact from './components/Contact';
+import NotFound from './components/NotFound';
 import Header from "./components/Header"
 import './App.css';
 
@@ -14,10 +14,10 @@ function App() {
       <Header/>
       <Switch>
       <Route exact path = "/" component = {GitHubIssuesList}/>
-      {/* <Route  exact path = "/issues/:id" component = {GitHubIssuesListItem}/>
+      <Route  exact path = "/issues/:id" component = {GitHubIssuesListItem}/>
       <Route exact path = "/About" component = {About} />
       <Route exact path = "/Contact" component = {Contact}/>
-      <Route component = {NotFound}/> */}
+      <Route component = {NotFound}/>
       </Switch>
     </div>
     </BrowserRouter>
